f757b040ddb322a71b7dcfd2afd1df96945318a0
Alex Bennett <alex_bennett@hotmail.com>
2019-12-06 00:57:00 +0000

    FE Build: Adding global styling and including in vue project

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="app">
+  <div class="grid" v-if="content">
     <Title :copy="content.title" />
-    <Body :copy="content.body"></Body>
+    <Body :copy="content.body" />
+    <Quote :copy="content.quote" />
+    <Feature :copy="content.feature" />
   </div>
 </template>
 
@@ -11,11 +13,15 @@ import axios from 'axios';
 
 import Title from './components/Title.vue';
 import Body from './components/Body.vue';
+import Quote from './components/Quote.vue';
+import Feature from './components/Feature.vue';
 
 @Component({
   components: {
     Title,
     Body,
+    Quote,
+    Feature,
   },
 })
 export default class App extends Vue {
